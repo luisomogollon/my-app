96c75998bf12db14181d2c0daacbf2e5587a9631
LouMogollonDev <91619676+luisomogollon@users.noreply.github.com>
2024-02-09 19:11:56 -0400

    agregar imagenes

---
+++ src/assets/img/imagenes.js
@@ -0,0 +1,8 @@
+import Prb from "./img/Prb_H.png";
+import icon from "./img/icon.png";
+
+/* eslint import/no-anonymous-default-export: [2, {"allowObject": true}] */
+export default {
+  img0: Prb,
+  img1: icon,
+};
