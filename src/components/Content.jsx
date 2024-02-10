075bcfc7092f3af3c480ba4c4994340834a53b96
LouMogollonDev <91619676+luisomogollon@users.noreply.github.com>
2024-02-10 10:47:02 -0400

    cambio 10:46

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,38 +1,20 @@
 import React from "react";
-import { Carousel } from 'react-responsive-carousel';
-import 'react-responsive-carousel/lib/styles/carousel.min.css'; 
 import Pic7 from "../assets/img/Pic7.png";
 import Pic8 from "../assets/img/Pic8.png";
 import Pic9 from "../assets/img/Pic9.png";
-
-
 import "./Main.css";
 
 const Content = () => {
   return (
-    <div className="sevenCard" >
-      <h1 id="VIEWALLSection" style={{ fontSize: "56px", color: "white"  }}>Lorem ipsum</h1>
-      
-      <Carousel 
-        showArrows={true}
-        showStatus={true}
-        showThumbs={false}
-        emulateTouch={true}
-        slidesToSlide={1}
-        centerSlidePercentage={37.3}
-        autoPlay={true}
-        swipeable={true}
-        centerMode={true}
-        
-      >
+    <div className="sevenCard">
+      <h1 style={{ fontSize: "56px", color: "white" }}>Lorem ipsum</h1>
+      <div style={{ display: "flex", gap: "5%" }}>
         <div>
           <img
             src={Pic7}
             alt="pic"
             style={{ objectFit: "fill", width: "300px", height: "400px" }}
-            
           />
-          
         </div>
         <div>
           <img
@@ -48,12 +30,21 @@ const Content = () => {
             style={{ objectFit: "fill", width: "300px", height: "400px" }}
           />
         </div>
-       
-        
-        
-      </Carousel>
-
-      <button 
+      </div>
+      <div
+        style={{
+          display: "flex",
+          marginTop: "2%",
+          width: "100%",
+          justifyContent: "center",
+          gap: "2%",
+        }}
+      >
+        <i className="fas fa-circle" style={{ color: "white" }}></i>
+        <i className="far fa-circle" style={{ color: "white" }}></i>
+        <i className="far fa-circle" style={{ color: "white" }}></i>
+      </div>
+      <button
         className="btnStandar"
         style={{ color: "black", backgroundColor: "white", marginTop: "5%" }}
       >
